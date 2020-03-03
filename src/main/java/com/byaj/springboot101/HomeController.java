4ff6ee722b74b1a49284e6fd156d1e73320d887e
AJ Henley <ajhenley@gmail.com>
2020-03-02 19:45:32 -0500

    Initial commit

---
+++ src/main/java/com/byaj/springboot101/HomeController.java
@@ -0,0 +1,16 @@
+package com.byaj.springboot101;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+
+        return "hometemplate";
+    }
+
+}
